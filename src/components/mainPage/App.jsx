aacea9b9431d8a55425c3e9b97b57c020558c9fe
sayasha <2two2two8eight@mail.ru>
2024-02-16 23:48:20 +0300

    added job layout

--- src/components/mainPage/App.jsx
+++ src/components/mainPage/App.jsx
@@ -4,6 +4,7 @@ import Reviews from './reviews/Reviews'
 import Advantages from './advantages/Advantages'
 import Discounts from './discounts/Discounts'
 import AffiliateProgram from './affiliateProgram/AffiliateProgram'
+import Job from './job/Job'
 
 const App = () => {
 	return (
@@ -15,6 +16,7 @@ const App = () => {
 			<Advantages />
 			<Discounts />
 			<AffiliateProgram />
+			<Job />
 		</div>
 		</>
 	)
---
+++ src/components/mainPage/job/Job.jsx
@@ -0,0 +1,19 @@
+import './job.css'
+
+const Job = () => {
+  return (
+	<div className="job">
+		<h2><span>Р</span>абота у нас</h2>
+		<div className="job_items">
+			<div className="job_title">
+				<p>Диагност электронных систем</p>
+				<h3>Открыта вакансия, ждём в свою команду!</h3>
+				<input type="submit" />
+			</div>
+			<img src="../../../../public/worker.png" alt="" />
+		</div>
+	</div>
+  )
+}
+
+export default Job
