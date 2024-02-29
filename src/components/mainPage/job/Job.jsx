6803315566be7a4e4af6aebece02f3c435c1fa65
sayasha <2two2two8eight@mail.ru>
2024-03-01 00:02:32 +0300

    a little upgrade

--- src/components/mainPage/job/Job.jsx
+++ src/components/mainPage/job/Job.jsx
@@ -5,12 +5,16 @@ import { jobData } from '../../job.data'
 const Job = () => {
 	const [jobItems, setJobItems] = useState(jobData)
 	return (
-		<div className="job" id='job'>
-			<h2><span>Р</span>абота у нас</h2>
-			{jobItems.length > 0 && (
-				jobItems.map((jobItem) => <JobItem key={jobItem.id} setJobItems={setJobItems} jobItem={jobItem}/>)
-			)}
-		</div>
+		<>
+		{jobItems.length > 0 ? (
+			<div className="job" id='job'>
+				<h2><span>Р</span>абота у нас</h2>
+				{jobItems.map((jobItem) => <JobItem key={jobItem.id} setJobItems={setJobItems} jobItem={jobItem}/>)}
+			</div>
+		) : (
+			console.log('first')
+		)}
+		</>
 	)
 }
 
