eb9cf43ce9729584001911a468c39d73816b3a27
Danil-114195722 <ejyou.user@gmail.com>
2024-03-04 18:34:36 +0300

    Temporary commented just for some days

--- backend/service_record/models.py
+++ backend/service_record/models.py
@@ -1,36 +1,36 @@
-from django.db import models
-
-from service.models import Service
-
-
-class ServiceRecord(models.Model):
-    service = models.ForeignKey(
-        to=Service,
-        on_delete=models.CASCADE,
-        null=False,
-        blank=False
-    )
-    car_brand = models.CharField(
-        max_length=100,
-        null=False,
-        blank=False
-    )
-    car_number = models.CharField(
-        max_length=10,
-        null=False,
-        blank=False
-    )
-    client_phone = models.CharField(
-        max_length=11,
-        null=False,
-        blank=False
-    )
-    viewed = models.BooleanField(
-        default=0
-    )
-    sent_at = models.DateTimeField(
-        auto_now_add=True,
-        null=False,
-        blank=False
-    )
-
+# from django.db import models
+#
+# from service.models import Service
+#
+#
+# class ServiceRecord(models.Model):
+#     service = models.ForeignKey(
+#         to=Service,
+#         on_delete=models.CASCADE,
+#         null=False,
+#         blank=False
+#     )
+#     car_brand = models.CharField(
+#         max_length=100,
+#         null=False,
+#         blank=False
+#     )
+#     car_number = models.CharField(
+#         max_length=10,
+#         null=False,
+#         blank=False
+#     )
+#     client_phone = models.CharField(
+#         max_length=11,
+#         null=False,
+#         blank=False
+#     )
+#     viewed = models.BooleanField(
+#         default=0
+#     )
+#     sent_at = models.DateTimeField(
+#         auto_now_add=True,
+#         null=False,
+#         blank=False
+#     )
+#
