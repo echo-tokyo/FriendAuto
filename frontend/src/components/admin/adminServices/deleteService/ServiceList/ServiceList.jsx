97cb0df401f1c104cb3f487e67f2162d641e6e39
sayasha <2two2two8eight@mail.ru>
2024-03-05 21:55:10 +0300

    added admin slice

--- frontend/src/components/admin/adminServices/deleteService/ServiceList/ServiceList.jsx
+++ frontend/src/components/admin/adminServices/deleteService/ServiceList/ServiceList.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react';
+import {useDispatch} from 'react-redux'
+import {toDelete} from '../../../../../store/admin/admin.slice'
 
 const ServiceList = ({category}) => {
-	const [selectedService, setSelectedService] = useState(null);
-
-	const toDel = (service) => {
-		const selectedElement = document.getElementById(service.id);
-		if (selectedElement) {
-			if (selectedElement.classList.contains('checked')) {
-				selectedElement.classList.remove('checked');
-				setSelectedService(null);
-			} else {
-				const prevSelectedElement = document.getElementById(selectedService);
-				if (prevSelectedElement) {
-					prevSelectedElement.classList.remove('checked');
-				}
-				selectedElement.classList.add('checked');
-				setSelectedService(service.id);
-			}
-		}
-	}
-
-	console.log(selectedService)
-
+	const dispatch = useDispatch()
 	return (
 		<div className="categories_with_services">
 			<h3 className='category'>{category.category_name}:</h3>
 			<div className='services_in_category'>
 				{category.services.map((service, index) => (
-					<h3 style={{whiteSpace: 'pre'}} key={service.id} id={service.id} onClick={() => toDel(service)}>{index === category.services.length - 1 ? `${service.service_name}.` : `${service.service_name}, `}</h3>
+					<h3 style={{whiteSpace: 'pre'}} key={service.id} id={service.id} onClick={() => dispatch(toDelete(service))}>{index === category.services.length - 1 ? `${service.service_name}.` : `${service.service_name}, `}</h3>
 				))}
 			</div>
 		</div>
---
+++ frontend/src/store/admin/admin.slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const adminSlice = createSlice({
+	name: 'admin',
+	initialState: {
+		selectedService: null
+	},
+	reducers: {
+		toDelete: (state, action) => {
+			const selectedElement = document.getElementById(action.payload.id);
+			if (selectedElement) {
+				if (selectedElement.classList.contains('checked')) {
+					selectedElement.classList.remove('checked');
+					state.selectedService = null;
+				} else {
+					const prevSelectedElement = document.getElementById(state.selectedService);
+					if (prevSelectedElement) {
+						prevSelectedElement.classList.remove('checked');
+					}
+					selectedElement.classList.add('checked');
+					state.selectedService = action.payload.id;
+				}
+			}
+		}
+	}
+})
+
+export const {toDelete} = adminSlice.actions
+export default adminSlice.reducer
