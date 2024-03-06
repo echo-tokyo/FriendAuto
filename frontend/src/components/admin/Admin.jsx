577209aa742a35d2ffd1a76f3bcd0c4b55a3f7a3
sayasha <2two2two8eight@mail.ru>
2024-03-07 00:11:04 +0300

    updated

--- frontend/src/components/admin/Admin.jsx
+++ frontend/src/components/admin/Admin.jsx
@@ -4,22 +4,32 @@ import Worksheets from './lists/worksheets/Worksheets'
 import AdminServices from './adminServices/AdminServices'
 import Vacansies from './vacansies/Vacansies'
 import AdminFooter from './adminFooter/AdminFooter'
+import { Link } from 'react-router-dom'
 
 const Admin = () => {
 	return (
 		<>
-		<div className="content">
-			<AdminHeader />
-			<div className="lists">
-				<Requests />
-				<Worksheets />
-			</div>
-		</div>
-		<AdminServices />
-		<div className="content">
-			<Vacansies />
-		</div>
-		<AdminFooter />
+		{localStorage.getItem('token') ? (
+			<>
+				<div className="content">
+					<AdminHeader />
+					<div className="lists">
+						<Requests />
+						<Worksheets />
+					</div>
+				</div>
+				<AdminServices />
+				<div className="content">
+					<Vacansies />
+				</div>
+				<AdminFooter />
+			</>
+		) : (
+			<>
+			<p>Вход не совершен</p>
+			<Link to='/'><h3>Вернуться на сайт</h3></Link>
+			</>
+		)}
 		</>
 	)
 }
