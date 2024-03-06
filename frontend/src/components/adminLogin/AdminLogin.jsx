74fc6338290bb22a9a6d486154fd3731efc397ae
sayasha <2two2two8eight@mail.ru>
2024-03-06 23:56:07 +0300

    added login request

--- frontend/src/components/adminLogin/AdminLogin.jsx
+++ frontend/src/components/adminLogin/AdminLogin.jsx
@@ -1,15 +1,35 @@
+import axios from 'axios'
 import './adminLogin.css'
+import { useNavigate } from 'react-router-dom'
 
 const AdminLogin = () => {
+	const nav = useNavigate()
+	const login = (e) => {
+		e.preventDefault()
+		const userData = {
+			username: e.target.login.value,
+			password: e.target.password.value
+		}
+		axios.post('http://188.225.36.185/api/user/login/', userData)
+		.then(response => {
+			localStorage.setItem('token', response)
+			nav('/admin/panel')
+		})
+		.catch(error => {
+			console.error('Ошибка при входе', error)
+			document.querySelector('.admin_login').style.border = '1px solid red'
+			console.log(document.querySelector('.admin_login'))
+		})
+	}
 	return (
 		<>
 		<div className="admin_background"></div>
 		<div className="wrapper">
 			<div className="admin_login">
 				<h2><span>В</span>ход</h2>
-				<form action="" className='admin_form'>
-					<input type="email" placeholder='Логин'/>
-					<input type="password" placeholder='Пароль'/>
+				<form action="" className='admin_form_l' onSubmit={(e) => login(e)}>
+					<input type="text" name='login' placeholder='Логин'/>
+					<input type="password" name='password' placeholder='Пароль'/>
 					<input type="submit" />
 				</form>
 			</div>
--- frontend/src/components/requests.data.js
+++ frontend/src/components/requests.data.js
@@ -5,6 +5,13 @@ const requests = [
 		car: 'Toyota',
 		number: 'a123aa',
 		phone: '+7 (999) 100 15 55'
+	},
+	{
+		id: 2,
+		name: 'Замена масла',
+		car: 'Toyota',
+		number: 'a123aa',
+		phone: '+7 (999) 100 15 55'
 	}
 ]
 
