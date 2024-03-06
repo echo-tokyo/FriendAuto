caa1b2d0e7894d65ba7a6f8d28a8856847826895
sayasha <2two2two8eight@mail.ru>
2024-03-06 22:16:11 +0300

    added scroll to top and some styles

--- frontend/src/components/admin/adminHeader/AdminHeader.jsx
+++ frontend/src/components/admin/adminHeader/AdminHeader.jsx
@@ -1,11 +1,12 @@
 import './adminHeader.css'
+import { Link } from 'react-router-dom'
 
 const AdminHeader = () => {
 	return (
 		<div className="header">
 			<div className='nav'>
-				<h3>Выйти</h3>
-				<h3>Вернуться на сайт</h3>
+				<Link to='/admin'><h3>Выйти</h3></Link>
+				<Link to='/'><h3>Вернуться на сайт</h3></Link>
 			</div>
 			<h2><span>Админ</span>-панель</h2>
 		</div>
--- frontend/src/components/mainPage/footer/Footer.jsx
+++ frontend/src/components/mainPage/footer/Footer.jsx
@@ -8,7 +8,7 @@ const footer = () => {
 				<h3 className='phone'>+7 (958) 880-00-08</h3>
 			</div>
 			<div className="footer_map">
-				<img src="../../../../public/map.png" alt="map" />
+				<a href="https://yandex.ru/maps/org/frend_avto/127785587044/reviews/?ll=39.609224%2C47.212061&z=14" target='_blank'><img src="../../../../public/map.png" alt="map" /></a>
 				<h3>Пр. Стачки, 264, Ростов-на-Дону</h3>
 			</div>
 		</footer>
