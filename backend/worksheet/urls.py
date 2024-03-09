c8366c9e31e7f8f24d132c4ab9e20ef7496d378e
Danil-114195722 <ejyou.user@gmail.com>
2024-03-09 22:53:18 +0300

    Added resources for worksheets

--- backend/worksheet/urls.py
+++ backend/worksheet/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 
+from .views import AddWorksheetAPIView, GetWorksheetsAPIView, MarkWorksheetViewedAPIView
+
+
 urlpatterns = [
+    path('get-worksheets/', GetWorksheetsAPIView.as_view()),
+
+    path('add-worksheet/', AddWorksheetAPIView.as_view()),
+    path('mark-viewed/', MarkWorksheetViewedAPIView.as_view()),
 ]
