7ec2fc2db9d75e1b332be779bcfe997b31c1b26a
sayasha <2two2two8eight@mail.ru>
2024-03-12 17:23:18 +0300

    count services

--- frontend/src/components/mainPage/services/servicesCards/ServicesCards.jsx
+++ frontend/src/components/mainPage/services/servicesCards/ServicesCards.jsx
@@ -1,14 +1,34 @@
 import { useNavigate } from 'react-router-dom'
 import '../services.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ServicesCards = () => {
 	const nav = useNavigate()
+	const [firstService, setFirstService] = useState('')
+	const [secondService, setSecondService] = useState('')
+	const [thirdService, setThirdService] = useState('')
+	const [fourthService, setFourthService] = useState('')
+	const [fifthervice, setFifthService] = useState('')
+ 
+	useEffect(() => {
+		axios.get('http://188.225.36.185/api/service/get-categories/')
+		.then((response) => {
+			setFirstService(response.data[0].count_services)
+			setSecondService(response.data[1].count_services)
+			setThirdService(response.data[2].count_services)
+			setFourthService(response.data[3].count_services)
+			setFifthService(response.data[4].count_services)
+		})
+	}, [])
+
 	return (
 		<div className="cards">
 			<div className="card" onClick={() => nav('/services/signup/1')}>
 				<img src="../../../../public/diagnostika.png" alt="" />
 				<div className="card_text">
 					<h3>Диагностика авто</h3>
+					<h4>Услуг: {firstService}</h4>
 					<h4>Подробнее</h4>
 				</div>
 			</div>
@@ -16,7 +36,7 @@ const ServicesCards = () => {
 				<img src="../../../../public/engine.png" alt="" />
 				<div className="card_text">
 					<h3>Ремонт двигателя</h3>
-					<h4>11 услуг</h4>
+					<h4>Услуг: {secondService}</h4>
 					<h4>Подробнее</h4>
 				</div>
 			</div>
@@ -24,7 +44,7 @@ const ServicesCards = () => {
 				<img src="../../../../public/hodovaya.png" alt="" />
 				<div className="card_text">
 					<h3>Ремонт ходовой части</h3>
-					<h4>6 услуг</h4>
+					<h4>Услуг: {thirdService}</h4>
 					<h4>Подробнее</h4>
 				</div>
 			</div>
@@ -32,7 +52,7 @@ const ServicesCards = () => {
 				<img src="../../../../public/kpp.png" alt="" />
 				<div className="card_text">
 					<h3>Ремонт КПП</h3>
-					<h4>4 услуги</h4>
+					<h4>Услуг: {fourthService}</h4>
 					<h4>Подробнее</h4>
 				</div>
 			</div>
@@ -40,7 +60,7 @@ const ServicesCards = () => {
 				<img src="../../../../public/kyzov.png" alt="" />
 				<div className="card_text">
 					<h3>Кузов и прочее</h3>
-					<h4>6 услуг</h4>
+					<h4>Услуг: {fifthervice}</h4>
 					<h4>Подробнее</h4>
 				</div>
 			</div>
