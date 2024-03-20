5d69c7f12cf6ed789dc867932dbe9a1491a24146
sayasha <2two2two8eight@mail.ru>
2024-03-21 00:32:50 +0300

    clickable number

--- frontend/src/components/servicesPage/questions/Questions.jsx
+++ frontend/src/components/servicesPage/questions/Questions.jsx
@@ -2,12 +2,12 @@ import './questions.css'
 
 const Questions = () => {
 	return (
-		<div className="questions">
+		<div className="questions" onClick={() => window.location.href='tel:89588800008'}>
 			<div className="questions_item1">
 				<h2>Остались вопросы?</h2>
 				<h3>Свяжитесь с нашим оператором</h3>
 			</div>
-			<h3>+7 (958) 880-00-08</h3>
+			<h3 className='number'>+7 (958) 880-00-08</h3>
 		</div>
 	)
 }
