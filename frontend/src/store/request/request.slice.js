4741ce4c65aebd5723a08e65ff3bbcef103a585a
sayasha <2two2two8eight@mail.ru>
2024-03-22 20:58:15 +0300

    bug fix

--- frontend/src/store/request/request.slice.js
+++ frontend/src/store/request/request.slice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const requestSlice = createSlice({
 	name: 'request',
 	initialState: {
-		ipAddress: 'http://188.225.36.185'
+		ipAddress: 'http://45.12.74.203'
 	}
 })
 
