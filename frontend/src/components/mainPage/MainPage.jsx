69bd51e6c53239b360b36313ffd0dfb5cad228ab
sayasha <2two2two8eight@mail.ru>
2024-03-22 21:41:31 +0300

    i hope its a bug fix

--- frontend/src/components/mainPage/MainPage.jsx
+++ frontend/src/components/mainPage/MainPage.jsx
@@ -29,24 +29,23 @@ const MainPage = () => {
 		})
 	}, [])
 
-	function onEntry(entry) {
-		entry.forEach(change => {
-			if (change.isIntersecting) {
-				change.target.classList.add('element-show')
-			}
-			console.log('change.target',change.target)
-		})
-	}
-	let options = {
-		threshold: [0.5]
-	}
-	let observer = new IntersectionObserver(onEntry, options)
-	let elements = document.querySelectorAll('.element-animation')
-	console.log(elements)
-	for (let elm of elements) {
-		observer.observe(elm)
-		console.log('elm', elm)
-	}
+	setTimeout(() => {
+		function onEntry(entry) {
+			entry.forEach(change => {
+				if (change.isIntersecting) {
+					change.target.classList.add('element-show')
+				}
+			})
+		}
+		let options = {
+			threshold: [0.5]
+		}
+		let observer = new IntersectionObserver(onEntry, options)
+		let elements = document.querySelectorAll('.element-animation')
+		for (let elm of elements) {
+			observer.observe(elm)
+		}
+	}, 100);
 
 	return (
 		<>
